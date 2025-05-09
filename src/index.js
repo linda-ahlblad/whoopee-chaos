70c24729cfbd06865062e6d7fed848a638530a05
Linda Ahlblad <linda@pauhu.com>
2025-05-09 12:40:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,14 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// Console branding
-console.log(`
-%c Whoopee Chaos %c v1.0.0 %c
-%c The divine flatulence game of mythical proportions!
-`,
-'background: #8e2de2; color: white; font-weight: bold; padding: 4px 0;',
-'background: #4a6da7; color: white; font-weight: normal; padding: 4px 0;',
-'background: transparent;',
-'color: #e94822; font-style: italic;'
-);
